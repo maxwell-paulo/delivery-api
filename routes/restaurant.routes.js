b60211c319659847bf1b22dedc86b1bce7118e9d
Maxwell Paulo <Maxwell@Maxwells-MacBook-Pro.local>
2022-09-22 17:02:23 -0300

    final lab

--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -1,5 +1,79 @@
 import express from "express";
+import { RestaurantModel } from "../models/restaurant.model.js";
 
 const restaurantRouter = express.Router();
+
+// Create
+
+restaurantRouter.post("/", async (req, res) => {
+  try {
+    const createdRestaurant = await RestaurantModel.create(req.body);
+
+    return res.status(201).json(createdRestaurant);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
+
+// Read all
+restaurantRouter.get("/", async (req, res) => {
+  try {
+    const allRestaurants = await RestaurantModel.find();
+
+    return res.status(200).json(allRestaurants);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
+
+//Read one
+restaurantRouter.get("/:id", async (req, res) => {
+  try {
+    const restaurant = await RestaurantModel.findOne({ _id: req.params.id });
+
+    return res.status(200).json(restaurant);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
+
+// Update
+
+restaurantRouter.put("/:id", async (req, res) => {
+  try {
+    const editRestaurant = await RestaurantModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(editRestaurant);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
+
+//DELETE
+restaurantRouter.delete("/:id", async (req, res) => {
+  try {
+    const deletedRestaurant = await RestaurantModel.deleteOne({
+      _id: req.params.id,
+    });
+
+    return res.status(200).json(deletedRestaurant);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
 
 export { restaurantRouter };
